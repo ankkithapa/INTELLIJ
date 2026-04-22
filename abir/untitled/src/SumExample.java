1eb0227c0ed73d186ec67b785bab0f7a4a20cc78
ankkithapa <ankithapamagar@gmail.com>
2026-04-22 15:16:31 +0900

    Update SumExample.java

--- abir/untitled/src/SumExample.java
+++ abir/untitled/src/SumExample.java
@@ -2,7 +2,7 @@ public class SumExample {
     public static void main(String[] args) {
         int sum = 0;
 
-        for(int i=1;i<=5;i++){
+        for(int i=1;i<100;i++){
             sum = sum +i;
         }
 
