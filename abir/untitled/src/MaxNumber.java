29d8b69ead5db2ce9663ae49aa2a84f38e51a9bd
ankkithapa <ankithapamagar@gmail.com>
2026-04-22 15:44:47 +0900

    Add files via upload

---
+++ abir/untitled/src/MaxNumber.java
@@ -0,0 +1,14 @@
+public class MaxNumber {
+    public static void main(String[] args) {
+        int[]nums={3,7,2,9,5};
+        int max=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
+        }
+
+        System.out.println("Max:"+max);
+    }
+}
